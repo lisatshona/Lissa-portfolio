19f3ee698e77171cbd85cb75afe51395e80991d3
Lissa Tshona <62402931+lisatshona@users.noreply.github.com>
2024-05-06 15:28:24 +0200

    form & validations

--- index.js
+++ index.js
@@ -14,3 +14,55 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+
+document.getElementById("downloadButton").addEventListener("click", function() {
+    // Replace 'path_to_your_cv.pdf' with the actual path to your CV file
+    var cvPath = 'CV_202404151040506.pdf';
+    var link = document.createElement("a");
+    link.href = cvPath;
+    link.download = "Your_CV_Name.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+function validate() { 
+    let name = 
+        document.getElementById("name").value; 
+    let subject = 
+        document.getElementById("subject").value; 
+    let phone = 
+        document.getElementById("phone").value; 
+    let email = 
+        document.getElementById("email").value; 
+    let message = 
+        document.getElementById("message").value; 
+    let error_message = 
+        document.getElementById("error_message"); 
+  
+    error_message.style.padding = "10px"; 
+  
+    let errors = []; 
+  
+    if (name.length < 5) { 
+        errors.push("Please Enter a valid Name");} 
+    if (subject.length < 10) { 
+        errors.push("Please Enter a Correct Subject");} 
+    if (isNaN(phone) || phone.length != 10) { 
+        errors.push("Please Enter a valid Phone Number");} 
+    if (email.indexOf("@") == -1 || email.length < 6) { 
+        errors.push( 
+            "Please Enter a valid Email");} 
+    if (message.length <= 40) { 
+        errors.push( 
+            "Please Enter More Than 40 Characters");} 
+  
+    if (errors.length > 0) { 
+        error_message.innerHTML = 
+            errors.join("<br>"); 
+        return false;} 
+    else { 
+        alert( 
+            "Form Submitted Successfully!"); 
+        return true;}}
+  
